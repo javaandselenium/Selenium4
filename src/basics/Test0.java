ca97948c990be9d8e230da6108fb0f2cdc4df3a6
QSP <QSP@QSP>
2021-05-11 18:01:18 +0530

    Basic of selenium

---
+++ src/basics/Test0.java
@@ -0,0 +1,17 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		//to luanch chrome browser
+	ChromeDriver driver=new ChromeDriver();
+		
+		//to launch the fireox browser
+	FirefoxDriver driver1=new FirefoxDriver();
+
+	}
+
+}
